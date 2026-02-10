48918cc68d922c446897db70a50272da2ce70722
Arjun KR <arjunkrishnaraj123@gmail.com>
2026-02-10 12:00:31 +0530

    fix: add TypeScript ESLint parser config for core package

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['plugin:react/recommended', 'turbo', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/jsx-key': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
